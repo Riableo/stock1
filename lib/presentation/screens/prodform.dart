6783a0671bbfa6079d5b0e5c653b0e2e0c28b00a
Leo <rbrian@academia.usbbog.edu.co>
2024-04-05 20:05:05 -0500

    Optimizing form overflowed

--- lib/presentation/screens/prodform.dart
+++ lib/presentation/screens/prodform.dart
@@ -46,116 +46,118 @@ class _ProdsState extends State<Prods> {
       body: Center(
         child: Form(
           key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Iconprods.getImage(_nameController.value.text) != '' ? Image.asset(Iconprods.getImage(_nameController.value.text), width: 100, height: 100,)
-              : const Icon(Icons.image_not_supported_rounded, size: 150,),
-              TextFormField(
-                controller: _idController,
-                enabled: widget.prodArgs?.id == null, // Deshabilitar si el ID se proporciona
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.numbers),
-                  hintText: 'i.e. 10',
-                  labelText: 'ID *',
-                ),
-                onSaved: (String? value) {
-                  id = int.parse(value!);
-                },
-              ),
-              TextFormField(
-                controller: _nameController,
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.shopping_bag_outlined),
-                  hintText: 'i.e. burger',
-                  labelText: 'Name *',
-                ),
-                onSaved: (String? value) {
-                  name = value;
-                },
-              ),
-              TextFormField(
-                controller: _stockController,
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.warehouse),
-                  hintText: 'i.e. 10',
-                  labelText: 'Stock *',
-                ),
-                onSaved: (String? value) {
-                  stock = int.parse(value!);
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: widget.prodArgs?.id != null ?  
-                ElevatedButton(
-                  onPressed: () async {
-                    // Validate returns true if the form is valid, or false otherwise.
-                    if (_formKey.currentState!.validate()) {
-
-                      _formKey.currentState!.save(); //save
-
-                      // Create a Dog and add it to the dogs table
-                      var fido = Prod(
-                        id: id,
-                        name: name.toString(),
-                        stock: stock,
-                      );
-                      
-                      await updateProd(fido);
-
-                      if (!context.mounted) return;
-                      Navigator.of(context).pop();
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Iconprods.getImage(_nameController.value.text) != '' ? Image.asset(Iconprods.getImage(_nameController.value.text), width: 100, height: 100,)
+                : const Icon(Icons.image_not_supported_rounded, size: 150,),
+                TextFormField(
+                  controller: _idController,
+                  enabled: widget.prodArgs?.id == null, // Deshabilitar si el ID se proporciona
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
                     }
+                    return null;
                   },
-                  child: const Text('Update'),
-                ) : // Deshabilitar si el ID se proporciona
-                ElevatedButton(
-                  onPressed: () async {
-                    // Validate returns true if the form is valid, or false otherwise.
-                    if (_formKey.currentState!.validate()) {
-
-                      _formKey.currentState!.save(); //save
-
-                      // Create a Dog and add it to the dogs table
-                      var fido = Prod(
-                        id: id,
-                        name: name.toString(),
-                        stock: stock,
-                      );
-                      
-                      await insertProd(fido);
-
-                      if (!context.mounted) return;
-                      Navigator.of(context).pop();
-                    }
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.numbers),
+                    hintText: 'i.e. 10',
+                    labelText: 'ID *',
+                  ),
+                  onSaved: (String? value) {
+                    id = int.parse(value!);
                   },
-                  child: const Text('Create'),
                 ),
-              ),
-            ],
-          ),
+                TextFormField(
+                  controller: _nameController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.shopping_bag_outlined),
+                    hintText: 'i.e. burger',
+                    labelText: 'Name *',
+                  ),
+                  onSaved: (String? value) {
+                    name = value;
+                  },
+                ),
+                TextFormField(
+                  controller: _stockController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.warehouse),
+                    hintText: 'i.e. 10',
+                    labelText: 'Stock *',
+                  ),
+                  onSaved: (String? value) {
+                    stock = int.parse(value!);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: widget.prodArgs?.id != null ?  
+                  ElevatedButton(
+                    onPressed: () async {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+
+                        _formKey.currentState!.save(); //save
+
+                        // Create a Dog and add it to the dogs table
+                        var fido = Prod(
+                          id: id,
+                          name: name.toString(),
+                          stock: stock,
+                        );
+                        
+                        await updateProd(fido);
+
+                        if (!context.mounted) return;
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    child: const Text('Update'),
+                  ) : // Deshabilitar si el ID se proporciona
+                  ElevatedButton(
+                    onPressed: () async {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+
+                        _formKey.currentState!.save(); //save
+
+                        // Create a Dog and add it to the dogs table
+                        var fido = Prod(
+                          id: id,
+                          name: name.toString(),
+                          stock: stock,
+                        );
+                        
+                        await insertProd(fido);
+
+                        if (!context.mounted) return;
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    child: const Text('Create'),
+                  ),
+                ),
+              ],
+            ),
+          )
         )
       )
     );
